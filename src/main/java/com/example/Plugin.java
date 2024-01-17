7918e4c4a3ce488fe02da306499afdb68dd8860c
adam-ht <todomek1234@gmail.com>
2024-01-17 22:17:48 +0100

    Version 2.0, added broadcasting message for players, and delay for shutting down after that time

--- src/main/java/com/example/Plugin.java
+++ src/main/java/com/example/Plugin.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 public class Plugin extends JavaPlugin {
 
@@ -17,6 +18,7 @@ public class Plugin extends JavaPlugin {
 
         config.addDefault("port:", 50000);
         config.addDefault("shutdown_command:", "SHUTDOWN");
+        config.addDefault("delay(in seconds):", 120);
         config.options().copyDefaults(true);
         saveConfig();
 
@@ -24,7 +26,7 @@ public class Plugin extends JavaPlugin {
 
         try {
             serverSocket = new ServerSocket(port);
-            getLogger().info("Plugin oczekuje na pakiety na porcie " + port);
+            getLogger().info("Plugin is listening on port " + port);
 
             new Thread(new Runnable() {
                 @Override
@@ -40,25 +42,36 @@ public class Plugin extends JavaPlugin {
                 }
             }).start();
         } catch (IOException e) {
-            getLogger().severe("Błąd podczas inicjalizacji serwera TCP: " + e.getMessage());
+            getLogger().severe("Error while initializing TCP server: " + e.getMessage());
         }
     }
 
     private void handlePacket(Socket clientSocket) {
         try {
+            int result = config.getInt("delay(in seconds):") / 60;
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String message = reader.readLine();
 
             if (message != null && message.equals(config.getString("shutdown_command:"))) {
-                getLogger().info("Otrzymano polecenie SHUTDOWN. Wyłączanie serwera...");
+                getLogger().info("Received command " + config.getString("shutdown_command:") + ". Shutting down the server...");
+                if (config.getInt("delay(in seconds):") < 60) {
+                    getServer().broadcastMessage("Server will be powered off in " + config.getInt("delay(in seconds):") + " seconds");
+                } else {
+                    getServer().broadcastMessage("Server will be powered off in " + result + " minutes");
+                }
+                
+                try{
+                TimeUnit.SECONDS.sleep(config.getInt("delay(in seconds):"));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 getServer().shutdown();
             } else {
-                getLogger().warning("Otrzymano nieznany pakiet: " + message);
+                getLogger().warning("Received unrecognized command: " + message);
             }
-
             clientSocket.close();
         } catch (IOException e) {
-            getLogger().severe("Błąd podczas obsługi pakietu: " + e.getMessage());
+            getLogger().severe("Error handling package: " + e.getMessage());
         }
     }
 
@@ -69,7 +82,7 @@ public class Plugin extends JavaPlugin {
                 serverSocket.close();
             }
         } catch (IOException e) {
-            getLogger().severe("Błąd podczas zamykania serwera TCP: " + e.getMessage());
+            getLogger().severe("Error shutting down TCP server: " + e.getMessage());
         }
     }
 }
